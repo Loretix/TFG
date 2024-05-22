04fbc7418cf8d38ac1f87ae550abfb134dfd6e35
Loretix <loretomatinero@gmail.com>
2024-05-22 12:01:02 +0200

    se ve la imagen

--- app/src/main/java/com/example/sanbotapp/ImageActivity.java
+++ app/src/main/java/com/example/sanbotapp/ImageActivity.java
@@ -8,6 +8,7 @@ import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -33,7 +34,20 @@ public class ImageActivity extends TopBaseActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // Mantener la pantalla encendida
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        // Ocultar la barra de navegación
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
         super.onCreate(savedInstanceState);
         onMainServiceConnected();
         setContentView(R.layout.imagen);
